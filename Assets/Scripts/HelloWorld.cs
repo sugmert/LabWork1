5422205c862ab7416572684b312826eabbca8fee
Sabri Ugur Mert <s.ugurmrt@gmail.com>
2024-02-28 14:41:06 +0300

    Kod

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorlds : MonoBehaviour
+{
+
+    void Start()
+    {
+        print("Hello World");
+    }
+}
